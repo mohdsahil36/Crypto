93013e185a8e31d1b6c4483b1bb46739a55c4f23
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2025-01-26 23:45:04 +0530

    landing page first layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import { cn } from "@/lib/utils";
-import { Spotlight } from "./ui-components/spotlight";
+// import { cn } from "@/lib/utils";
+import { Spotlight } from "./ui-components/Spotlight";
 
 export default function Home() {
   return (
     <div className="h-screen max-w-[80%] mx-auto dark:bg-black rounded-3xl ">
       <>
         <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center dark:bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-          {/* <Spotlight /> */}
-          <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-            <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text dark:text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          <Spotlight />
+          <div className=" p-4 max-w-7xl mx-auto relative z-10  w-full pt-20 md:pt-0">
+            <h1 className="text-4xl md:text-6xl font-bold text-center bg-clip-text dark:text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
             Effortless Crypto Tracking <br /> Without the Guesswork
             </h1>
             <p className="mt-4 font-normal text-lg dark:text-neutral-300 max-w-lg text-center mx-auto">
