2c323a4b7fe4ea654d5f65fd889afd37063728f5
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2025-02-10 01:05:30 +0530

    Landing page first layout- dashboard redirect button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,31 @@
 "use client"
 import React from 'react';
 import { Spotlight } from './ui-components/spotlight-new';
+import { FlipWords } from './ui-components/flip-words';
+import { Button } from './ui-components/moving-border';
+import { ArrowRight } from 'lucide-react';
 
 export default function Home() {
+  const words = ["Simplified for Clarity", "Done with Ease", "Without the Guesswork"];
   return (
     <div>
       <div className="h-screen w-full flex md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight />
         <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-10 md:pt-40">
           <h1 className="text-4xl md:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            Effortless Crypto Tracking <br /> Without the Guesswork
+            Effortless Crypto Tracking <br /> <FlipWords words={words} className="text-center" />
           </h1>
           <p className="mt-4 font-normal text-md md:text-lg text-neutral-300 max-w-lg text-center mx-auto">
-          Advanced users stay ahead of the market by exploring real-time data, trends, and insights on a seamless platform
+            Advanced users stay ahead of the market by exploring real-time data, trends, and insights on a seamless platform
           </p>
+          <div className='text-center mt-8 md:mt-10'>
+            <Button
+              className="bg-white dark:bg-black dark:hover:bg-zinc-800 ease-in-out duration-500 text-black dark:text-white border-neutral-200 dark:border-slate-800 flex gap-x-2"
+            >
+              Dashboard
+              <ArrowRight size={18}/>
+            </Button>
+        </div>
         </div>
       </div>
     </div>
