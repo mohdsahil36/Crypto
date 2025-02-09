0ac522870e862038dc06ed351d7d86a52b9a3050
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2025-02-10 01:29:15 +0530

    Landing page first layout- tailwind stylings, typeerrors corrections

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <div className="h-screen w-full flex md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight />
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-10 md:pt-40">
+        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-10 md:pt-52">
           <h1 className="text-4xl md:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
             Effortless Crypto Tracking <br /> <FlipWords words={words} className="text-center" />
           </h1>
